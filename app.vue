24aef75ab6dffb55c5a891fb46ff1b66c4860f10
Ankit Srivastava <67647761+ankitsrivas14@users.noreply.github.com>
2024-01-08 22:22:34 +0530

    updated app for layout

---
+++ app.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="wrapper">
+    <Sidebar />
+    <NuxtPage class="app-page_container"/>
+    <Drawer />
+  </div>
+</template>
+
+<style>
+
+@import './assets/css/global.scss';
+
+.wrapper{
+  display: flex;
+  height: 100%;
+  .app-page_container{
+    margin-left: 260px;
+  }
+}
+
+</style>
