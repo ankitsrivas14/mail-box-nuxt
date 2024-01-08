a04e305cdc263a55b7e87f7cd863dfb2d957098a
Ankit Srivastava <67647761+ankitsrivas14@users.noreply.github.com>
2024-01-08 22:21:33 +0530

    component for page heading

---
+++ components/PageHeading.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="page-heading">
+      <slot />
+    </div>
+  </template>
+  
+  <style>
+  
+ .page-heading{
+    color: #121829;
+    font-size: 32px;
+    font-weight: 500;
+    padding: 24px 24px 32px;
+ }
+  
+  </style>
+  
