1964a8241cf2fd04053a7b81ebb08f1f11d1e7f6
Ankit Srivastava <67647761+ankitsrivas14@users.noreply.github.com>
2024-01-08 22:21:55 +0530

    created mail actions and mail mail items component

---
+++ components/MailActions.vue
@@ -0,0 +1,39 @@
+<template>
+    <div class="mail-actions_wrapper">
+        <div class="mail-actions_text">
+            <input type="checkbox" />
+            <span>Email Selected (12)</span>
+        </div>
+        <div class="mail-actions_btns">
+            <span class="mail-actions_btns__btn">Mark as read (r)</span>
+            <span class="mail-actions_btns__btn">Archive (a)</span>
+        </div>
+    </div>
+</template>
+
+<script setup>
+</script>
+
+<style lang="scss">
+    .mail-actions_wrapper{
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        padding: 0 24px 24px;
+        font-weight: 500;
+        font-size: 14px;
+        .mail-actions_text{
+            display: flex;
+            align-items: center;
+            gap: 12px;
+        }
+        .mail-actions_btns{
+            display: flex;
+            gap: 40px;
+            align-items: center;
+            .mail-actions_btns__btn{
+                cursor: pointer;
+            }
+        }
+    }
+</style>
---
+++ components/MailItem.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="mail-item_wrapper">
+        <input type="checkbox" />
+        <span>Wave hello with video, reactions and more now in huddles</span>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss">
+    .mail-item_wrapper{
+        display: flex;
+        align-items: center;
+        padding: 24px;
+        border-top: 1px solid #E6E7EB;
+        gap: 12px;
+        font-weight: 500;
+        font-size: 14px;
+        cursor: pointer;
+        transition: background 0.3s ease;
+        &:hover{
+            background: #D1E2FF;
+        }
+    }
+</style>
