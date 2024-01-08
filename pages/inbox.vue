3c5bc11df06d2e7947dd917ff696d23d621b6b57
Ankit Srivastava <67647761+ankitsrivas14@users.noreply.github.com>
2024-01-08 22:22:19 +0530

    created inbox page UI

---
+++ pages/inbox.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="page_wrapper">
+        <div class="page-header">
+            <PageHeading>Inbox</PageHeading>
+            <MailActions />
+        </div>
+        <MailItem />
+        <MailItem />
+        <MailItem />
+        <MailItem />
+        <MailItem />
+        <MailItem />
+        <MailItem />
+        <MailItem />
+        <MailItem />
+        <MailItem />
+        <MailItem />
+        <MailItem />
+        <MailItem />
+    </div>
+</template>
+<style lang="scss">
+.page_wrapper{
+    width: 100%;
+    height: 100%;
+    .page-header{
+        position: sticky;
+        top: 0;
+        background-color: #FFFFFF;
+    }
+}
+</style>
+  
